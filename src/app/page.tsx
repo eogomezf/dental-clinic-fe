f6c5816c7b00f3aff88010446d4fbd218aee124c
AlvisonHunterArnuero <alvison@gmail.com>
2025-05-17 21:19:06 -0600

    refactor(page): Add initial login screen layout with two-column design and animated footer image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,157 @@
-import Image from "next/image";
+import Image from 'next/image';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import LoginIcon from '@mui/icons-material/Login';
+import React from 'react';
+import {
+  Box,
+  Button,
+  Paper,
+  Stack,
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+} from '@mui/material';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Container className="flex flex-col items-center h-screen justify-center min-h-screen p-4 ">
+      <Stack
+        display={'flex'}
+        direction={'row'}
+        className="items-center justify-between gap-2 w-full"
+      >
+        <Box className="hidden h-full w-0 sm:w-[30%] md:w-[50%] lg:w-full md:flex items-center justify-center  transition-all duration-500 ease-in-out">
+          <Image
+            aria-hidden
+            src="/dentora-pro-login-screen.png"
+            alt="Dentora Pro Logon Screen"
+            priority
+            width={515}
+            height={785}
+            className="
+            border rounded-lg
+      transition-all duration-500 ease-in-out
+      lg:opacity-100
+      md:opacity-50
+      sm:opacity-30
+      opacity-0
+      md:hover:opacity-85
+      md:hover:scale-95
+      md:hover:border-4
+      md:hover:border-sky-500
+      md:hover:rounded-4xl
+        lg:translate-x-0
+      md:translate-x-[-50%]
+      sm:translate-x-[-100%]
+    "
+          />
+        </Box>
+        <Stack
+          display={'flex'}
+          direction={'column'}
+          spacing={1}
+          justifyContent={'center'}
+          alignItems={'center'}
+          className="h-screen w-full flex items-center justify-center"
+          sx={{
+            backgroundImage: {
+              sm: 'url(/dentora-pro-login-screen-02.png)',
+              md: 'none',
+            },
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        >
+          <Box className="flex items-center gap-2">
+            <Paper sx={{ padding: 0.5 }} elevation={1}>
+              <HealthAndSafetyIcon
+                sx={{
+                  fontSize: '3rem',
+                }}
+              />
+            </Paper>
+            <Typography
+              className="text-5xl font-bold text-sky-500 text-center"
+              variant="h3"
+              component="h1"
+              fontWeight={700}
+              fontSize={'3rem'}
+              fontFamily={'var(--font-inter)'}
+            >
+              Dentora Pro
+            </Typography>
+          </Box>
+          <Box className="flex flex-col items-center justify-center">
+            <Typography
+              variant="body1"
+              fontFamily={'var(--font-inter)'}
+              sx={{ width: '100%', maxWidth: '600px' }}
+              textAlign={'center'}
+              fontSize={'1rem'}
+              color={'#ffffff'}
+              fontWeight={400}
+              className="text-center"
+              marginTop={2}
+              marginBottom={4}
+            >
+              Dentora Pro is a powerful and flexible web application
+              designed to streamline appointment management for dental
+              clinics, offering an intuitive and efficient scheduling
+              experience.
+            </Typography>
+            <Button
+              href="/login"
+              className="mt-4"
+              variant="outlined"
+              startIcon={<LoginIcon />}
+            >
+              Login
+            </Button>
+          </Box>
+        </Stack>
+      </Stack>
+      <footer className="absolute bottom-0 right-0 left-0 flex items-center justify-center"></footer>
+      <AppBar
+        position="fixed"
+        sx={{ top: 'auto', bottom: 0 }}
+        color="transparent"
+      >
+        <Toolbar>
+          <Typography
+            variant="body2"
+            style={{
+              display: 'flex',
+              textAlign: 'center',
+              gap: '0.5rem',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+              fontFamily: 'var(--font-inter-ui)',
+              fontSize: '0.875rem',
+              fontWeight: 400,
+              color: '#ffffff',
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            © {new Date().getFullYear()} Proudly created by
+            <a
+              className="pl- flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://codecrafterslabs.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                aria-hidden
+                src="/globe.svg"
+                alt="Globe icon"
+                width={16}
+                height={16}
+              />
+              codecrafterslabs.com →
+            </a>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Container>
   );
 }
