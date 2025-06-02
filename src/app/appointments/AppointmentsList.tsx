88358574404ce5442db24ff57ab73d7a993cff8f
Elvis Gomez <eogomezf@gmail.com>
2025-06-01 20:23:48 -0600

    Make adaptations for listing Appointments

---
+++ src/app/appointments/AppointmentsList.tsx
@@ -0,0 +1,88 @@
+import { Appointment } from "@/app/data/appointments-data";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Box, Tooltip } from "@mui/material";
+import Container from "@mui/material/Container";
+import Sheet from "@mui/joy/Sheet";
+import EditCalendar from "@mui/icons-material/EditCalendar";
+import Delete from "@mui/icons-material/Delete"; // Uncomment if you have a delete icon
+// import { appointments } from "@/app/data/appointments-data";
+
+function AppointmentsList({ appointments }: { appointments: Appointment[] }) {
+  return (
+    <Container className="flex flex-col items-center   justify-center py-4 ">
+      <Box>
+        <Sheet sx={{ height: 300, overflow: "auto" }}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 1000 }} aria-label="simple table">
+              <TableHead>
+                <TableRow sx={{ backgroundColor: "#1976d2", color: "#fff" }}>
+                  <TableCell>Title </TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell align="center">Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {appointments.map((appointment) => (
+                  <TableRow
+                    key={appointment.title}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="appointment">
+                      {appointment.title}
+                    </TableCell>
+
+                    <TableCell>{appointment.description}</TableCell>
+                    <TableCell>
+                      {" "}
+                      {appointment.startTime.toLocaleDateString()}{" "}
+                      {appointment.startTime.toLocaleTimeString()} -{" "}
+                      {appointment.endTime.toLocaleTimeString()}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Tooltip title="Edit Appointment">
+                        <button className="text-blue-500 hover:underline">
+                          <EditCalendar sx={{ color: "green" }} />
+                        </button>
+                      </Tooltip>
+                      <Tooltip title="Delete Appointment">
+                        <button className="text-red-500 hover:underline ml-4">
+                          <Delete />
+                        </button>
+                      </Tooltip>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Sheet>
+      </Box>
+    </Container>
+  );
+
+  //   return (
+  //     <div>
+  //       {appointments.map((appointment) => (
+  //         <div key={appointment.title} className="appointment-item">
+  //           <h3>{appointment.title}</h3>
+  //           <p>{appointment.description}</p>
+  //           <p>
+  //             {appointment.startTime.toLocaleDateString()}{" "}
+  //             {appointment.startTime.toLocaleTimeString()} -{" "}
+  //             {appointment.endTime.toLocaleTimeString()}
+  //           </p>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   );
+}
+
+export default AppointmentsList;
--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import AppointmentsList from "./AppointmentsList";
+import { appointments } from "@/app/data/appointments-data";
+import NavBar from "./NavBar";
+import Container from "@mui/material/Container";
+
+//import { getAppointments } from "@/app/data/appointments-data";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <NavBar />
+
+      <Container className="flex flex-col items-center justify-center mt-15 py-4">
+        <h2 className="text-2xl font-bold mb-4">Dentora Pro Appointments</h2>
+        <p className="text-gray-600 mb-8">
+          Here are your upcoming appointments. Click on an appointment to view
+          more details or to edit it.
+        </p>
+        <AppointmentsList appointments={appointments} />
+      </Container>
+    </>
+  );
 }
