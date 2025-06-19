32ae22081fbf8a2eed296f4834462be40b3483cc
Jose Lopez <acunalopezjosenahum@gmail.com>
2025-06-18 20:17:24 -0600

    remove address and phone validators - use regex validator with name and lastName to ensure that input contains only letters or '.

--- src/app/components/Forms/ValidationSchemas.ts
+++ src/app/components/Forms/ValidationSchemas.ts
@@ -1,3 +1,4 @@
+import { match } from 'assert';
 import * as Yup from 'yup';
 
 export const SignInSchema = Yup.object().shape({
@@ -11,20 +12,21 @@ export const SignInSchema = Yup.object().shape({
 
 export const SignUpSchema = Yup.object().shape({
   firstName: Yup.string()
+    .matches(/^[A-Za-zÀ-ÿ\s'-]+$/,'It must contained just letters')
     .min(2, 'Too Short!')
     .max(50, 'Too Long!')
     .required('Name is Required'),
   lastName: Yup.string()
+    .matches(/^[A-Za-zÀ-ÿ\s'-]+$/,'It must contained just letters')
     .min(2, 'Too Short!')
     .max(50, 'Too Long!')
     .required('Last name is Required'),
   address: Yup.string()
     .min(5, 'Address too short')
-    .max(100, 'Address too long')
-    .required('Address is required'),
+    .max(100, 'Address too long'),
   phone: Yup.string()
-    .matches(/^\+?[0-9\s\-]{8,9}$/, 'Invalid phone number')
-    .required('Phone number is required'),
+    .matches( /^(\+?\d{1,3}[-.\s]?|\()?(\d{1,4})\)?[-.\s]?(\d{1,4})[-.\s]?(\d{1,9})$/,
+       'Invalid phone number'),
   email: Yup.string()
     .email('Invalid email')
     .required('Email is Required'),
