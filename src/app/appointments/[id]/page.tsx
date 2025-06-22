10e6fbf79a4acb459ef97834928d00a1bb0b771a
Elvis Gomez <eogomezf@gmail.com>
2025-06-21 22:18:22 -0600

    Add edit page

---
+++ src/app/appointments/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { Appointment } from "@/app/models/appointments.model";
+import FormEditAppointment from "../../components/Forms/FormEditAppointment";
+import { fetchAppointments } from "@/app/services/appointments.service";
+
+async function Editpage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const { id } = await params;
+  const appointmentsFetched = await fetchAppointments();
+  const appointments = appointmentsFetched.appointments || [];
+  const appointment = appointments.find((a: Appointment) => a.id == id);
+
+  return (
+    <div>
+      <FormEditAppointment appointment={appointment} />
+    </div>
+  );
+}
+
+export default Editpage;
