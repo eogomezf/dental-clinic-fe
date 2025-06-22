d1e76978682126b0e5f8814ef95847731d2937b7
Bismarck Gomez <gfbismarck@gmail.com>
2025-06-21 22:56:11 -0600

    Add appointet fetch service, validating date, overlap appoinmet concidences by date and hour, validate business hour, update alerts

--- src/app/components/Forms/FormEditAppointment.tsx
+++ src/app/components/Forms/FormEditAppointment.tsx
@@ -4,19 +4,15 @@ import { useEffect, useState } from "react";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
+import { TextField } from "@mui/material";
 import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
 import SendIcon from "@mui/icons-material/Send";
-import { BE_URL } from "@/lib/config";
-import { Snackbar, Alert, AlertColor, Button, Grid } from "@mui/material";
-
-interface Appointment {
-  _id: string;
-  title: string;
-  description: string;
-  startTime: string | Date;
-  endTime: string | Date;
-}
+import { Button, Grid, Alert, Snackbar } from "@mui/material";
+import {
+  EditAppointment,
+  fetchAppointments,
+} from "../../services/appointments.service";
+import { Appointment } from "@/app/models/appointments.model";
 
 interface AppointmentProp {
   appointment: Appointment;
@@ -25,12 +21,19 @@ interface AppointmentProp {
 function FormEditAppointment({ appointment }: AppointmentProp) {
   const router = useRouter();
   const [open, setOpen] = useState(false);
-  const [message, setMessage] = useState("");
-  const [severity, setSeverity] = useState<AlertColor>("success");
+  const [error, setError] = useState("");
+  const [errorStartTime, setErrorStartTime] = useState("");
+  const [errorEndTime, setErrorEndTime] = useState("");
+  const [statusSubmit, setStatusSubmit] = useState(false);
 
-  const mostrarAlerta = (mensaje: string, tipo: AlertColor) => {
-    setMessage(mensaje);
-    setSeverity(tipo);
+  const [message, setMessage] = useState("");
+
+  const [severity, setSeverity] = useState<
+    "success" | "error" | "info" | "warning"
+  >("success");
+
+  const showMessage = (alertMessage: string) => {
+    setMessage(alertMessage);
     setOpen(true);
   };
 
@@ -49,9 +52,9 @@ function FormEditAppointment({ appointment }: AppointmentProp) {
     setForm({
       title: appointment.title || "",
       description: appointment.description || "",
-      date: start.toISOString().split("T")[0], // YYYY-MM-DD
-      startTime: start.toTimeString().slice(0, 5), // HH:mm
-      endTime: end.toTimeString().slice(0, 5), // HH:mm
+      date: start.toISOString().split("T")[0],
+      startTime: start.toTimeString().slice(0, 5),
+      endTime: end.toTimeString().slice(0, 5),
     });
   }, [appointment]);
 
@@ -60,8 +63,91 @@ function FormEditAppointment({ appointment }: AppointmentProp) {
     handleUpdate();
   };
 
+  const validateDate = (value: string) => {
+    const today = new Date().toISOString().split("T")[0];
+    if (value < today) {
+      setError("You cannot set a date before today");
+      setStatusSubmit(true);
+    } else {
+      setError("");
+      setStatusSubmit(false);
+    }
+  };
+
+  const validateTime = async (field: string, value: string) => {
+    const lunchStart = "12:00";
+    const lunchEnd = "13:00";
+    const minTime = "08:00";
+    const maxTime = "17:00";
+    const msg = "You cannot set this time for";
+
+    if (field === "startTime") {
+      if (
+        value < minTime ||
+        value > maxTime ||
+        (value >= lunchStart && value <= lunchEnd)
+      ) {
+        setErrorStartTime(`${msg} ${field}`);
+        setStatusSubmit(true);
+      } else {
+        if (form.endTime && value > form.endTime) {
+          setErrorStartTime("The start time cannot be greater than end time");
+          setStatusSubmit(true);
+        } else {
+          setErrorStartTime("");
+          setStatusSubmit(false);
+          const appointmentsFetched = await fetchAppointments();
+          const appointments = appointmentsFetched.appointments || [];
+          const appointmentFinded = appointments.find(
+            (a: Appointment) =>
+              new Date(a.startTime).toISOString().split("T")[0] == form.date &&
+              new Date(a.startTime).toTimeString().slice(0, 5) == value
+          );
+          if (appointmentFinded) {
+            showMessage(
+              "An appointment already exists on the selected date and time"
+            );
+            setSeverity("warning");
+            setStatusSubmit(true);
+          } else {
+            setStatusSubmit(false);
+          }
+        }
+      }
+    }
+
+    if (field === "endTime") {
+      if (
+        value < minTime ||
+        value > maxTime ||
+        (value >= lunchStart && value <= lunchEnd)
+      ) {
+        setErrorEndTime(`${msg} ${field}`);
+        setStatusSubmit(true);
+      } else {
+        if (form.startTime && value < form.startTime) {
+          setErrorEndTime("The end time cannot be less than start time");
+          setStatusSubmit(true);
+        } else {
+          setErrorEndTime("");
+          setErrorStartTime("");
+          setStatusSubmit(false);
+        }
+      }
+    }
+  };
+
   const handleChange =
     (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      if (field === "date") {
+        validateDate(value);
+      }
+
+      if (field === "startTime" || field === "endTime") {
+        validateTime(field, value);
+      }
+
       setForm({ ...form, [field]: e.target.value });
     };
 
@@ -69,27 +155,25 @@ function FormEditAppointment({ appointment }: AppointmentProp) {
     const { title, description, date, startTime, endTime } = form;
     const start = new Date(`${date}T${startTime}`);
     const end = new Date(`${date}T${endTime}`);
-    const res = await fetch(`${BE_URL}/${appointment._id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title,
-        description,
-        startTime: start,
-        endTime: end,
-      }),
-    });
+    const IdAppointment = appointment.id;
+
+    appointment = {
+      id: IdAppointment,
+      title,
+      description,
+      startTime: start,
+      endTime: end,
+    };
+
+    const res = await EditAppointment(appointment.id, appointment);
 
     if (res.ok) {
-      mostrarAlerta("Datos guardados correctamente", "success");
-      setTimeout(() => {
-        router.refresh();
-        router.push("/appointments");
-      }, 2500);
+      showMessage("The appointment has been updated successfully");
+      setSeverity("success");
+      router.push("/appointments");
     } else {
-      mostrarAlerta("Hubo un error al guardar", "error");
+      showMessage("The appointmet was not updated");
+      setSeverity("error");
     }
   };
 
@@ -101,19 +185,26 @@ function FormEditAppointment({ appointment }: AppointmentProp) {
         mx: "auto",
         mt: 5,
         pb: 5,
+        border: "solid 1px gray",
         borderRadius: 2,
-        bgcolor: "#fff",
-        color: "#000",
+        bgcolor: "#ffffee",
+        color: "primary",
       }}
     >
       <>
         <form onSubmit={handleSubmit}>
           <Typography
             variant="h5"
+            color="primary"
             gutterBottom
             sx={{ textAlign: "center", pt: 4 }}
           >
-            Please select from the below options:
+            {appointment.user
+              ? "Editing Appointment of " +
+                appointment.user?.firstName +
+                " " +
+                appointment.user?.lastName
+              : "Editing Appointment"}
           </Typography>
           <Box display="flex" justifyContent="center" gap={2} m={5}>
             <TextField
@@ -125,43 +216,58 @@ function FormEditAppointment({ appointment }: AppointmentProp) {
               onChange={handleChange("title")}
               error={form.title.trim() === ""}
               helperText={
-                form.title.trim() === "" ? "El título es obligatorio" : ""
+                form.title.trim() === "" ? "The title is required" : ""
               }
             />
             <Grid display="flex" flexDirection={"column"} sx={{ width: "50%" }}>
               <TextField
                 fullWidth
-                required
                 id="date"
                 type="date"
                 value={form.date}
                 onChange={handleChange("date")}
                 label="Select the date"
+                error={!!error}
+                helperText={error || " "}
               />
             </Grid>
           </Box>
 
           <Box display="flex" justifyContent="center" gap={2} m={5}>
             <Grid display="flex" flexDirection={"column"} sx={{ width: "50%" }}>
-              <label> Enter your Start Time:</label>
+              <Typography
+                color="primary"
+                gutterBottom
+                sx={{ textAlign: "left" }}
+              >
+                Enter your Start Time:
+              </Typography>
               <TextField
+                fullWidth
                 type="time"
                 id="starttime"
                 value={form.startTime}
                 onChange={handleChange("startTime")}
-                fullWidth
-                required
+                error={!!errorStartTime}
+                helperText={errorStartTime || " "}
               />
             </Grid>
             <Grid display="flex" flexDirection={"column"} sx={{ width: "50%" }}>
-              <label>Enter your End Time:</label>
+              <Typography
+                color="primary"
+                gutterBottom
+                sx={{ textAlign: "left" }}
+              >
+                Enter your End Time:
+              </Typography>
               <TextField
+                fullWidth
                 type="time"
                 id="endtime"
                 value={form.endTime}
                 onChange={handleChange("endTime")}
-                fullWidth
-                required
+                error={!!errorEndTime}
+                helperText={errorEndTime || " "}
               />
             </Grid>
           </Box>
@@ -178,7 +284,7 @@ function FormEditAppointment({ appointment }: AppointmentProp) {
               error={form.description.trim() === ""}
               helperText={
                 form.description.trim() === ""
-                  ? "La descripción es obligatoria"
+                  ? "Please enter a description"
                   : ""
               }
             />
@@ -189,7 +295,6 @@ function FormEditAppointment({ appointment }: AppointmentProp) {
               endIcon={<ReplyOutlinedIcon />}
               variant="contained"
               onClick={() => {
-                router.refresh();
                 router.push("/appointments");
               }}
             >
@@ -200,22 +305,19 @@ function FormEditAppointment({ appointment }: AppointmentProp) {
               sx={{ marginRight: "50px" }}
               endIcon={<SendIcon />}
               variant="contained"
+              disabled={statusSubmit}
             >
               SUBMIT
             </Button>
 
             <Snackbar
+              sx={{ width: "100%" }}
               open={open}
               autoHideDuration={6000}
               onClose={() => setOpen(false)}
               anchorOrigin={{ vertical: "top", horizontal: "center" }}
             >
-              <Alert
-                onClose={() => setOpen(false)}
-                severity={severity}
-                variant="filled"
-                sx={{ width: "100%" }}
-              >
+              <Alert variant="filled" severity={severity}>
                 {message}
               </Alert>
             </Snackbar>
