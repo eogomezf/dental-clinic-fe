1e5e17326b9491ca4dbb423293fc5758c1a67a8d
Elvis Gomez <eogomezf@gmail.com>
2025-06-25 16:14:50 -0600

    Add filter not show admin user

--- src/app/add-appointments/page.tsx
+++ src/app/add-appointments/page.tsx
@@ -1,13 +1,15 @@
 import FormCreateAppointment from "../components/Forms/FormCreateAppointment";
+import { User } from "../models/users.model";
 import { fetchUsers } from "../services/users.service";
 
 async function Createpage() {
   const usersData = await fetchUsers();
   const users = usersData.allUsers || [];
+  const filtred = users.filter((user: User) => user.role !== "admin");
 
   return (
     <div>
-      <FormCreateAppointment usersList={users} appointmentsList={[]} />
+      <FormCreateAppointment usersList={filtred} appointmentsList={[]} />
     </div>
   );
 }
