ca6f85ca460b78955390d33074ea518fb385d050
Elvis Gomez <eogomezf@gmail.com>
2025-06-25 23:19:43 -0600

    redirect and token issue

--- src/app/action/sigupActions.ts
+++ src/app/action/sigupActions.ts
@@ -1,26 +1,38 @@
-'use server';
-
-import { SignUpFormValues } from '@/app/components/Forms/Forms.types';
+"use server";
+import { cookies } from "next/headers";
+import { SignUpFormValues } from "@/app/components/Forms/Forms.types";
 
 export const signupAction = async (data: SignUpFormValues) => {
+  let response;
+
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
-      method: 'POST',
+    response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
 
     if (!response.ok) {
       const error = await response.json();
-      throw new Error(error.message || 'Signup failed');
+      throw new Error(error.message || "Signup failed");
     }
 
     const result = await response.json();
-    return result;
+
+    const cookie = await cookies();
+    cookie.set("jwt_token", result.token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 1,
+      path: "/",
+      sameSite: "lax",
+    });
+
+    return "ok";
   } catch (error) {
-    console.error('An error occurred during signup:', error);
+    console.error("An error occurred during signup:", error);
     throw error;
   }
 };
