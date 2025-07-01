7d1e01d70a0fee3bcdde168cb61cca6f638cb60a
Bismarck Gomez <gfbismarck@gmail.com>
2025-06-30 18:43:11 -0600

    mergin changes from develop-test

--- src/app/appointments/AppointmentsList.tsx
+++ src/app/appointments/AppointmentsList.tsx
@@ -364,25 +364,32 @@ function AppointmentsList({
               )}
             </TableBody>
             <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                  colSpan={3}
-                  count={appointments.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  slotProps={{
-                    select: {
-                      inputProps: {
-                        "aria-label": "rows per page",
+              {filteredAppointments.length > 6 ? (
+                <TableRow>
+                  <TablePagination
+                    rowsPerPageOptions={[
+                      5,
+                      10,
+                      25,
+                      { label: "All", value: -1 },
+                    ]}
+                    colSpan={3}
+                    count={appointments.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    slotProps={{
+                      select: {
+                        inputProps: {
+                          "aria-label": "rows per page",
+                        },
+                        native: true,
                       },
-                      native: true,
-                    },
-                  }}
-                  onPageChange={handleChangePage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-              </TableRow>
+                    }}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                  />
+                </TableRow>
+              ) : null}
             </TableFooter>
           </Table>
         </Sheet>
